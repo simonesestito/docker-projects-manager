9fdd94f8328fe10c15f3674156991d80a55afbc1
Simone Sestito <simone@simonesestito.com>
2022-12-15 10:12:27 +0100

    Print_err: fix string concatenation

--- src/utils/print.py
+++ src/utils/print.py
@@ -20,7 +20,7 @@ def print_status(message: str):
     print(f'\n\033[96;1m{message}\033[0m')
 
 def print_ok(message: str):
-    print('\033[92;1m✓\033[0m ' + message)
+    print(f'\033[92;1m✓\033[0m {message}')
 
 def print_err(message: str):
-    print('\033[91;1m✕\033[0m ' + message)
+    print(f'\033[91;1m✕\033[0m {message}')
