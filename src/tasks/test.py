83f227de8742b6d851fc486ecb48fe40158f9541
Simone Sestito <simone@simonesestito.com>
2022-12-15 09:00:03 +0100

    Tests: wait a few moments and retry before failing the test

--- src/tasks/test.py
+++ src/tasks/test.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with "Docker Projects Manager".  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import requests
+import requests, time
 from config import DOCKER_HOST_PROXY_PORT_VAR_NAME
 from data.model import Project
 from utils.network import is_port_open
@@ -25,18 +25,27 @@ from utils.print import print_err, print_ok, print_status
 
 def test_project(project: Project):
     print_status(f'Running tests on {project.name}')
+    NUM_TRIES, TRY_SLEEP = 5, 0.5
 
-    if is_port_open(project.port):
-        print_ok(f'Port {project.port} is listening')
+    # Test port listening
+    for _ in range(NUM_TRIES):
+        time.sleep(TRY_SLEEP)
+        if is_port_open(project.port):
+            print_ok(f'Port {project.port} is listening')
+            break
     else:
         print_err(f'Be sure to listen on {project.port} (env var = {DOCKER_HOST_PROXY_PORT_VAR_NAME})')
     
+    # Test HTTP connection
     try:
-        http_response = requests.get(f'https://{project.domain_name}/')
-        if http_response.status_code == 502:
-            print_err('HTTP 502 Bad Gateway trying to connect via domain name. Make sure it didn\'t crash.')
+        for _ in range(NUM_TRIES):
+            time.sleep(TRY_SLEEP)
+            http_response = requests.get(f'http://localhost:{project.port}')
+            if http_response.status_code != 502:
+                print_ok(f'HTTP request with result {http_response.status_code}')
+                break
         else:
-            print_ok(f'HTTP request with result {http_response.status_code}')
+            print_err('HTTP 502 Bad Gateway trying to connect via localhost. Make sure it didn\'t crash.')
     except Exception as e:
         print_err(e)
         print_err('Unable to connect to domain, double check DNS settings for ' + project.domain_name)
